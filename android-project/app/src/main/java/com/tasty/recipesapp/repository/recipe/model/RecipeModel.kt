6e59641ebb71830f15ea651fe59aa2753ef2792f
Nagy Krisztian <61684038+nagykirsztian@users.noreply.github.com>
2023-12-14 14:05:36 +0200

    nevermind

--- android-project/app/src/main/java/com/tasty/recipesapp/repository/recipe/model/RecipeModel.kt
+++
@@ -1,9 +0,0 @@
-package com.tasty.recipesapp.repository.recipe.model
-
-data class RecipeModel(
-    val id: Int,
-    val name: String,
-    val description: String,
-    val thumbnailUrl: String,
-
-    )
--- android-project/app/src/main/java/com/tasty/recipesapp/repository/recipe/model/RecipesDTO.kt
+++
@@ -1,6 +0,0 @@
-package com.tasty.recipesapp.repository.recipe.model
-
-data class RecipesDTO(
-    val count: Int,
-    val results: List<RecipeDTO>
-)
